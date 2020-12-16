b74c36297f1bc3788334e851175381d2bafb927a
MarcosMiranda00 <mm0014032019@unab.edu.sv>
2020-12-15 19:43:36 -0600

    Revert "Revert "Add files via upload""
    This reverts commit 9e2b4f25d0b5de567c15050a2e04e31e3031ab3f.

---
+++ conexion/Compras.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.quick.delivery.conexion;
+
+/**
+ *
+ * @author user
+ */
+public class Compras {
+   protected int idCompra;
+   protected int idVenta;
+   protected int idCliente;
+   
+}
---
+++ conexion/Usuarios.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.unab.edu.quick.delivery.conexion;
+
+/**
+ *
+ * @author user
+ */
+public class Usuarios {
+  protected int idUsuario;
+  protected String nombreU;
+  protected String apellidoU;
+  protected String correoU;
+  protected String contraU;
+  protected String telefonoU;
+  protected String direccionU;
+  
+}
